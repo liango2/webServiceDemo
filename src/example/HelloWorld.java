138b810df077af5e1619263c21375f4a13ae3a67
liango2 <540625404@qq.com>
2015-08-09 00:20:55 +0800

    first commit

---
+++ src/example/HelloWorld.java
@@ -0,0 +1,12 @@
+package example;/**
+ *  @author liango 
+ *  @version 1.0
+ *  @since 2015-08-08 23:49
+ */
+public class HelloWorld {
+  public String sayHelloWorldFrom(String from) {
+    String result = "Hello, world, from " + from;
+    System.out.println(result);
+    return result;
+  }
+}
